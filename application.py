e6502514c0f5db5692baa23f0db2f39f92dc6aa1
Darijo Bakota <darijo.bakota@gmail.com>
2019-11-06 09:24:11 -0300

    Login form working correctly

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -21,10 +22,35 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+
+@app.route("/register", methods=["POST","GET"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+
+        #Check if all fields were filled in
+        if not request.form.get("name"):
+            return render_template("sorry.html", error="Username field not filled in")
+
+        elif not request.form.get("password"):
+            return render_template("sorry.html", error="Password field not filled in")
+
+        elif not request.form.get("passwordConfirm"):
+            return render_template("sorry.html", error="Password confirmation field not filled in")
+
+        #Get all fields from the form
+        name = request.form.get("name")
+        password = request.form.get("password")
+        passwordConfirm = request.form.get("passwordConfirm")
+
+        #Check form for correctness
+        if not password == passwordConfirm:
+            return render_template("sorry.html", error="Passwords did not match")
+
+        return render_template("sucess.html", name=name)
+    else:
+        return render_template("register.html")
